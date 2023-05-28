64a485c55f5e93c824b19aa282cb313b34005447
Abeer Taha <104815752+Abeertaha@users.noreply.github.com>
2023-05-28 11:48:43 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Art 
+{
+public interface IMusic
+{
+    void Play();
+}
+
+public abstract class Media : IMusic
+{
+    public string? Title { get; set; }
+    public abstract void Play();
+}
+
+public class Music : Media
+{
+    public string? Artist { get; set; }
+    public override void Play() => Console.WriteLine($"Playing music: {Title} by {Artist}");
+}
+
+public class Movie : Media
+{
+    public string? Director { get; set; }
+    public override void Play() => Console.WriteLine($"Playing movie: {Title} directed by {Director}");
+}
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        Media media1 = new Music { Title = "Wherever I May Roam", Artist = "Metallica" };
+        Media media2 = new Movie { Title = "Stay", Director = "Marc Forster" };
+
+        media1.Play();
+        media2.Play();
+    }
+}
+}
